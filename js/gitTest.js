73f9923b0bbf0ff5af5a49ad3be9338789caa3f8
Kang Taehoon <jadetypehoon@gmail.com>
2019-07-25 22:26:16 +0900

    Bugfix finished

--- js/gitTest.js
+++ js/gitTest.js
@@ -1,9 +1,31 @@
-Class Tree {
-  let left = undefined;
-  let right = undefined;
-  {
-    if (left === undefined || right === undefined){
-      console.log("문제가 있습니다")
+class Tree {
+  constructor(left, right) {
+    this.left = left;
+    this.right = right;
+  }
+
+  checkBody() {
+    if (this.left === undefined || this.right === undefined) {
+      console.log(this, " <= 문제가 있습니다");
     }
   }
 }
+
+const myTree = new Tree("왼팔", "오른팔");
+myTree.checkBody();
+
+const disorderTree = new Tree("왼팔");
+disorderTree.checkBody();
+
+
+/*
+class AnswerVer {
+  constructor(value) {
+    this.value = value;
+  }
+
+  get() {
+    return this.value;
+  }
+}
+ */
