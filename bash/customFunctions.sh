c028273b2e9e19808a7be22597287ee47121712a
HibikeQuantum <jadetypehoon@gmail.com>
2022-08-18 14:37:56 +0900

    Add custom function for my .zshrc this code will be loaded at initialize shell

---
+++ bash/customFunctions.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+##########################
+# $1 should be target full path
+# for example "/Users/kth/Code/PlayGround/Bear"
+##########################
+
+# echo "hello bash"
+# bash --version
+
+publisherPath="/Users/kth/Code/Bear-Auto-Publisher"
+
+canChangeDirectory () {
+    currentPath=`pwd`
+    targetPath=$1
+
+    echo "pwd:" $currentPath
+    echo "target:" $targetPath
+    echo "${currentPath}" "${targetPath}"
+    if [ "${currentPath}" = "${targetPath}" ]; then
+        echo 0
+    else 
+        echo 1
+    fi
+}
+
+_postBlogAutoCommit() {
+    targetPath="/Users/kth/Code/HibikeQuantum.github.io"
+    commitMessage="Build and auto commit "`date`
+    mypwd=`pwd`
+    cd $targetPath
+    bundle exec jekyll build; 
+    git add -A; git commit -m "${commitMessage}"; 
+    git push origin main;
+    cd $mypwd
+}
+_postBlogAutoCommit() {
+    targetPath="/Users/kth/Code/HibikeQuantum.github.io"
+    commitMessage="Build and auto commit "`date`
+    mypwd=`pwd`
+    cd $targetPath
+    bundle exec jekyll build; 
+    git add -A; git commit -m "${commitMessage}"; 
+    git push origin main;
+    cd $mypwd
+}
+
+
+_autoBearPublish() {
+    mypwd=`pwd`
+    cd publisherPath
+    sh AutoPublish.sh
+    cd $mypwd
+}
